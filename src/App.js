9e9ca609723bd4c45756984471bea12ac8de8ed7
Emil Guluzade <emilquluzade2001@gmail.com>
2023-05-22 17:33:42 +0400

    Added Edit, Remove items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-import Items from './Components/Items/Items';
 import AddInput from './Components/AddInput/AddInput';
 import React, { useState } from 'react';
+import Item from './Components/Item/Item';
 
+
+const startValues = [
+  {
+    isChecked: false,
+    description: "Work",
+    id: 0
+  }
+]
 
 function App() {
-
-  const startValues = [
-    {
-      id: 0,
-      description: "Work",
-      isChecked: true
-    }
-  ]
-
   const [tasks, setTasks] = new useState(startValues);
+
+  function removeTask(index){
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== index));
+  }
+  
+  function updateTaskDescription (index, newDescription){
+    const newTasks = tasks.map(item =>
+      {
+        if(item.id === index)
+        {
+          return {...item, description: newDescription}
+        }
+      })
+    setTasks(newTasks)
+  }
 
   function addTask(task)
   {
@@ -26,20 +39,46 @@ function App() {
 
   function taskIsChecked(index, isChecked)
   {
-      // setTasks((lastArray)=>{
-      //   lastArray[index].isChecked = isChecked;
-      //   return lastArray;
-      // })
+    const newTasks = tasks.map(item =>
+      {
+        if(item.id === index)
+        {
+          return {...item, isChecked: isChecked}
+        }
+       else if(item.id !== index) {
+          return item;
+        }
+      })
+    setTasks(newTasks)
   }
 
-  function removeTask(index){
-    // setTasks((prevArray)=>[])
+  function removeAllTasks()
+  {
+    setTasks([])
+  }
+  function removeCheckedTasks()
+  {
+    setTasks(prevTasks => prevTasks.filter(task => !task.isChecked));
+    
   }
 
   return (
     <div className="App">
       <AddInput taskAdded = {addTask} />
-      <Items allTasks = {tasks} taskChecked={taskIsChecked}/>
+      <div className='all-items'>
+      {
+        tasks.map((item, index) => 
+        <Item key = {index} taskRemoved = {removeTask} taskDescriptionUpdated = {updateTaskDescription} taskChecked={taskIsChecked} task ={item}/>)
+      }
+      </div>
+
+      <button onClick={removeAllTasks} className='button'>
+        Delete all tasks
+      </button>
+      <button onClick={removeCheckedTasks} className='button'>
+        Delete done tasks
+      </button>
+        
     </div>
   );
 }
---
+++ src/Components/Item/Item.js
@@ -0,0 +1,65 @@
+import React, { memo, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Item.css';
+
+function Item(props) {
+  const [showEditPanel, setshowEditPanel] = useState(false)
+  const [newDescription, setnewDescription] = useState('')
+
+  console.log(props)
+
+  function checkboxClicked()
+  {
+    props.taskChecked(props.task.id, !props.task.isChecked);
+  }
+
+  function editClicked()
+  {
+    setshowEditPanel(prevValue => !prevValue)
+  }
+
+  function handleNewDescriptionChange(event)
+  {
+    setnewDescription(event.target.value)
+  }
+
+  function descriptionChanged()
+  {
+    props.taskDescriptionUpdated(props.task.id, newDescription);
+    setshowEditPanel(false)
+  }
+
+  function deleteTask()
+  {
+    props.taskRemoved(props.task.id);
+  }
+
+  return (
+        // props.task && props.task.isChecked && props.task.description &&
+        props &&
+        <div className="card-body add-input-container ">
+            <p className="card-text">{props.task.id + ' ' + props.task.description}</p>
+          <button className='button'>
+            <input className="form-check-input" checked={props.task.isChecked} onChange={checkboxClicked} type="checkbox" value ={props.task.isChecked} id="flexCheckDefault"/>
+          </button>
+
+          <button onClick={editClicked} className='button'>
+            <img src='./edit.png' /> 
+          </button>
+          
+          <button onClick={deleteTask} className='button'>
+            <img src='./trash.png' /> 
+          </button>
+          {showEditPanel && <div>
+            <input 
+            onChange={handleNewDescriptionChange}
+            type="text"     
+            id="fname" 
+            name="firstname" 
+            placeholder="New Todo"></input>
+            <button className="add-button" onClick={descriptionChanged}>Change Description</button>
+          </div>}
+        </div>
+   );
+}
+export default Item;
